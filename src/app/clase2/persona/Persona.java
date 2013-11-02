f9f684e9c6a2889dcaea0a7f06af2527696bf845
alumno <alumno@LAB706-01>
2013-11-02 18:26:18 -0500

    Primera Version

---
+++ src/app/clase2/persona/Persona.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.clase2.persona;
+
+/**
+ *
+ * @author alumno
+ */
+public class Persona {
+
+    public String nombre;
+    public String apellidos;
+    public byte edad;
+
+    public Persona(String nombre, String apellidos, byte edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+}
