c6e3f940f7b198c06b6678ded6e8dff163695d84
alumno <alumno@LAB706-01>
2013-11-02 18:37:37 -0500

    Primera Version

--- src/app/clase2/persona/Persona.java
+++ src/app/clase2/persona/Persona.java
@@ -12,12 +12,40 @@ public class Persona {
 
     public String nombre;
     public String apellidos;
-    public byte edad;
+    public int edad;
 
-    public Persona(String nombre, String apellidos, byte edad) {
+ 
+            
+    public Persona(String nombre, String apellidos, int edad) {
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.edad = edad;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    
+    
 }
